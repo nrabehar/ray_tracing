e1bf41f430df2f4633d527468c480cbfc3fa403f
Nekena <safnekena1@gmail.com>
2025-03-27 00:26:28 +0300

    Add color normalization and conversion functions

--- src/color/color.c
+++ src/color/color.c
@@ -20,3 +20,33 @@ t_color	cl_had(t_color c1, t_color c2)
 {
 	return (cl_mul(c1, c2));
 }
+
+t_color	cl_norm(t_color c)
+{
+	t_range	cr;
+
+	cr = range(0.0, 255.0);
+	c = vec_scale(c, 255.0);
+	c.x = clamp(cr, c.x);
+	c.y = clamp(cr, c.y);
+	c.z = clamp(cr, c.z);
+	return (c);
+}
+
+t_color	cl_unit(t_color c)
+{
+	return (vec_div_scale(c, 255.0f));
+}
+
+int	cl_to_int(t_color c)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	c = cl_norm(c);
+	r = (int)c.x;
+	g = (int)c.y;
+	b = (int)c.z;
+	return (r << 16 | g << 8 | b);
+}
--- src/color/inc/color.h
+++ src/color/inc/color.h
@@ -1,12 +1,17 @@
 #ifndef COLOR_H
-#define COLOR_H
+# define COLOR_H
 
-#include "../../math/inc/math.h"
+# include "../../math/inc/math.h"
 
-typedef t_vec t_color;
+typedef t_vec	t_color;
 
-t_color color(double r, double g, double b);
-t_color cl_mul(t_color c1, t_color c2);
-t_color cl_had(t_color c1, t_color c2);
+t_color			color(double r, double g, double b);
+t_color			cl_mul(t_color c1, t_color c2);
+t_color			cl_had(t_color c1, t_color c2);
+
+t_color			cl_norm(t_color c);
+t_color			cl_unit(t_color c);
+
+int				cl_to_int(t_color c);
 
 #endif
