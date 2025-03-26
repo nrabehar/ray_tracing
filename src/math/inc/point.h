6d0bf75fb1f8bd4dbe289b9bb12c9b631487a238
Nekena <safnekena1@gmail.com>
2025-03-26 22:58:14 +0300

    Add point header and implementation for 3D point creation

---
+++ src/math/inc/point.h
@@ -0,0 +1,10 @@
+#ifndef POINT_H
+# define POINT_H
+
+# include "vector.h"
+
+typedef t_vec	t_pt;
+
+t_pt			pt3(double x, double y, double z);
+
+#endif
---
+++ src/math/point/point.c
@@ -0,0 +1,6 @@
+#include "minirt.h"
+
+t_pt	pt3(double x, double y, double z)
+{
+	return (vec3(x, y, z));
+}
