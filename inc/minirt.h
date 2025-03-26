c7cae840e5e022bb0b2ff687dcd638dba4b776c3
Nekena <safnekena1@gmail.com>
2025-03-26 23:18:22 +0300

    Add minirt header and main implementation for miniRT entry point

---
+++ inc/minirt.h
@@ -0,0 +1,11 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "../mlx/mlx.h"
+# include "../src/canvas/inc/canvas.h"
+# include "../src/color/inc/color.h"
+# include "../src/math/inc/math.h"
+# include "../src/win/inc/win.h"
+# include <stdio.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "minirt.h"
+
+int	main(void)
+{
+	printf("Hello miniRT\n");
+	t_vec v = vec3(1, 2, 3);
+	vec_print(v);
+	return (0);
+}
